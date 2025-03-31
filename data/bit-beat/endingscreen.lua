03ba1b3a68294c5531ed2731d8eac6afbb8a6605
SA64 <sa64ds@outlook.com>
2025-03-30 23:56:15 -0400

    April Fools Update Progress 3.30

--- data/bit-beat/endingscreen.lua
+++ data/bit-beat/endingscreen.lua
@@ -1,23 +1,28 @@
-function onSongStart()
-   endscreen=0
-   picoyeah=0
+function onCreate()
+   --All this was borrowed from SFG
+   setProperty('camFollowPos.x',650)
+   setProperty('camFollowPos.y',520)
+   setProperty('camFollow.x',650)
+   setProperty('camFollow.y',520)
+   setProperty('isCameraOnForcedPos',true)
+
 end
 
 function onEndSong()
-   endscreen=1
    playMusic('breakfast-(pico)')
-   makeLuaText('aprilFoolsHeading','Happy April Fools!')
-   addLuaText('aprilFoolsHeading')
+   openCustomSubstate('endingscreen')
    return Function_Stop
 end
 
-function onKeyPress(key)
-   if endscreen==1 then
-      endSong()
-   end
+function onCustomSubstateCreate(endingscreen)
+   makeLuaSprite('endscreensprite','endscreen')
+   insertToCustomSubstate('endscreensprite')
 end
 
-function onUpdate()
+function onCustomSubstateUpdate(endingscreen)
+   if (getPropertyFromClass('flixel.FlxG', 'keys.justPressed.enter') or getPropertyFromClass('flixel.FlxG', 'keys.justPressed.p')) and endscreen==1 then
+      endSong()
+   end
    if picoyeah==6 then
       loadSong('Bit-Beat (Pico Mix)', 1)
    end
--- stages/stage-smb.lua
+++ stages/stage-smb.lua
@@ -1,9 +1,8 @@
 function onCreate()
 	-- background shit
-	makeAnimatedLuaSprite('smb1stage', 'stages/sa64stage', 138, 46, 'aseprite');
+	makeLuaSprite('smb1stage', 'sa64stage', 138, 46);
 	setScrollFactor('smb1stage', 1, 1);
 	scaleObject('smb1stage', 4, 4);
-
 
 	addLuaSprite('smb1stage', false);
 	setProperty('smb1stage.antialiasing', false)
